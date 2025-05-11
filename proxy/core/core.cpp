0a26cbe27ad5d4393b199ba5a32e4f52784975f9
andrei <user@LAPTOP-H01KL82F>
2025-05-11 15:08:21 -0400

    ready_basic

--- proxy/core/core.cpp
+++ proxy/core/core.cpp
@@ -8,50 +8,68 @@
 extern void filter_any_server_async_recv(std::function<void(uint32_t, std::shared_ptr<std::string>, std::string)> handler);
 extern void filter_any_server_add_data(uint32_t any_server_id, const std::string& data, std::string protocol, std::string from_address, unsigned short from_port);
 extern bool filter_any_get_original_relation(uint32_t id, std::string proto, uint32_t& sa, uint16_t& sp, uint32_t& da, uint16_t& dp);
-extern void internet_connector_async_send(uint16_t source_id, uint8_t proto, uint32_t dest_address, uint16_t dest_port, std::shared_ptr<std::string> data);
-extern void internet_connector_async_receive(std::function<void(uint16_t, uint8_t, std::shared_ptr<std::string>)> handler);
+//extern void internet_connector_async_send(uint16_t source_id, uint8_t proto, uint32_t dest_address, uint16_t dest_port, std::shared_ptr<std::string> data);
+//extern void internet_connector_async_receive(std::function<void(uint16_t, uint8_t, std::shared_ptr<std::string>)> handler);
+
+extern void internet_connector_async_send(uint32_t id, uint8_t proto, uint32_t dest_address,
+    uint16_t dest_port, std::shared_ptr<std::string> data);
+
+extern void internet_connector_async_receive(std::function<void(uint32_t, std::shared_ptr<std::string>)> handler);
 
 struct any_relation_map_entry_t {
+    bool _null = true;
+    uint32_t source_id;
     uint32_t relation_id;
+    uint8_t proto;
 };
 
 struct any_relation_map_t {
-    void add(uint16_t source_id, uint32_t relation_id) {
-        any_relation_map_entry_t entry = { relation_id };
-        relation_mapping[source_id] = entry;
+    void add(uint32_t source_id, uint32_t relation_id, uint8_t proto) {
+        if (relation_mapping.find(source_id) == relation_mapping.end()) {
+            any_relation_map_entry_t entry = { false, source_id, relation_id , proto };
+            relation_mapping[source_id] = entry;
+        }
     }
 
-    bool get(uint16_t source_id, uint32_t& relation_id) {
+    any_relation_map_entry_t get(uint32_t source_id) {
         auto it = relation_mapping.find(source_id);
         if (it != relation_mapping.end()) {
-            relation_id = it->second.relation_id;
-            return true;
+            return it->second;
         }
-        return false;
+        return any_relation_map_entry_t();
+    }
+
+    any_relation_map_entry_t get_by_remaped_id(uint32_t remaped_id) {
+        for (auto& pair : relation_mapping) {
+            if (pair.second.relation_id == remaped_id) {
+                return pair.second;
+            }
+        }
+        return any_relation_map_entry_t();
     }
 
 private:
-    std::map<uint16_t, any_relation_map_entry_t> relation_mapping;
+    std::map<uint32_t, any_relation_map_entry_t> relation_mapping;
 };
 
 any_relation_map_t relation_map;
 
-void on_local_data(uint32_t relation_id, std::shared_ptr<std::string> data, std::string protocol) {
+void on_local_data(uint32_t source_id, std::shared_ptr<std::string> data, std::string protocol) {
     uint32_t sa, da;
     uint16_t sp, dp;
     // get original endpoint relations
-    if (filter_any_get_original_relation(relation_id, protocol, sa, sp, da, dp)) {
+    if (filter_any_get_original_relation(source_id, protocol, sa, sp, da, dp)) {
         uint8_t proto_value = (protocol == "tcp") ? 6 : 17; // 6 para TCP, 17 para UDP
-        relation_map.add(sp, relation_id); // mapeia o source_id para o relation_id
+        relation_map.add(source_id, sp, proto_value); // mapeia o source_id para o relation_id
         internet_connector_async_send(sp, proto_value, da, dp, data);
     }
 }
 
-void on_internet_data(uint16_t source_id, uint8_t proto, std::shared_ptr<std::string> data) {
-    uint32_t relation_id;
-    if (relation_map.get(source_id, relation_id)) {
-        std::string protocol = (proto == 6) ? "tcp" : "udp";
-        filter_any_server_add_data(relation_id, *data, protocol, "", 0);
+void on_internet_data(uint32_t internet_id, std::shared_ptr<std::string> data) {
+    auto _f = relation_map.get_by_remaped_id(internet_id);
+    if (!_f._null) {
+        std::string protocol = (_f.proto == 6) ? "tcp" : "udp";
+        filter_any_server_add_data(_f.source_id, *data, protocol, "", 0);
     }
     else {
         // lidar com o caso em que o source_id não está mapeado
--- proxy/test/internet_connector.cpp
+++ proxy/test/internet_connector.cpp
@@ -3,20 +3,31 @@
 #include <queue>
 #include <functional>
 #include <map>
-
+#include <iostream>
 
 class internet_connector {
 private:
     struct connection_data {
-        uint16_t source_id;
+        uint32_t source_id;
         uint8_t proto;
         uint32_t dest_address;
         uint16_t dest_port;
         std::shared_ptr<std::string> data;
     };
 
+    struct connection {
+        std::shared_ptr<boost::asio::ip::tcp::socket> socket;
+        boost::asio::ip::tcp::endpoint endpoint;
+        std::queue<std::shared_ptr<connection_data>> send_queue;
+        std::array<uint8_t, (uint16_t)-1> receive_buffer;
+        bool sending = false;
+        bool receiving = false;
+        bool connecting = false;
+        bool connected = false;
+    };
+
     internet_connector(boost::asio::io_context& io_context)
-        : io_context_(io_context), socket_(std::make_shared<boost::asio::ip::tcp::socket>(io_context)) {}
+        : io_context_(io_context) {}
 
     ~internet_connector() {}
 
@@ -26,7 +37,7 @@ public:
         return instance;
     }
 
-    void async_send(uint16_t source_id, uint8_t proto, uint32_t dest_address, uint16_t dest_port, std::shared_ptr<std::string> data) {
+    void async_send(uint32_t source_id, uint8_t proto, uint32_t dest_address, uint16_t dest_port, std::shared_ptr<std::string> data) {
         auto connection_data_ptr = std::make_shared<connection_data>();
         connection_data_ptr->source_id = source_id;
         connection_data_ptr->proto = proto;
@@ -34,48 +45,124 @@ public:
         connection_data_ptr->dest_port = dest_port;
         connection_data_ptr->data = data;
 
-        send_queue_[source_id].push(connection_data_ptr);
+        auto it = connections_.find(source_id);
+        if (it == connections_.end()) {
+            connections_[source_id] = std::make_shared<connection>();
+            connections_[source_id]->endpoint = boost::asio::ip::tcp::endpoint(boost::asio::ip::address_v4(dest_address), dest_port);
+            establish_connection(source_id);
+        }
 
+        connections_[source_id]->send_queue.push(connection_data_ptr);
         do_send(source_id);
     }
 
-    void async_receive(std::function<void(uint16_t, uint8_t, std::shared_ptr<std::string>)> handler) {
+    void async_receive(std::function<void(uint32_t, std::shared_ptr<std::string>)> handler) {
         receive_handler_ = handler;
-        // Implementação da recepção de dados...
+        for (auto& connection : connections_) {
+            do_receive(connection.first);
+        }
     }
 
 private:
-    void do_send(uint16_t source_id) {
-        if (send_queue_[source_id].empty()) {
+    void establish_connection(uint32_t source_id) {
+        auto it = connections_.find(source_id);
+        if (it == connections_.end() || !it->second) {
             return;
         }
 
-        auto connection_data_ptr = send_queue_[source_id].front();
-        send_queue_[source_id].pop();
+        auto connection_ptr = it->second;
+        if (connection_ptr->connecting || connection_ptr->connected) {
+            return;
+        }
 
-        boost::asio::ip::tcp::endpoint endpoint(boost::asio::ip::address_v4(connection_data_ptr->dest_address), connection_data_ptr->dest_port);
+        if (!connection_ptr->socket) {
+            connection_ptr->socket = std::make_shared<boost::asio::ip::tcp::socket>(io_context_);
+        }
 
-        socket_->async_connect(endpoint, [this, connection_data_ptr](const boost::system::error_code& error) {
+        connection_ptr->connecting = true;
+        connection_ptr->socket->async_connect(connection_ptr->endpoint, [this, source_id](const boost::system::error_code& error) {
+            auto it = connections_.find(source_id);
+            if (it == connections_.end() || !it->second) {
+                return;
+            }
+
+            auto connection_ptr = it->second;
+            connection_ptr->connecting = false;
             if (!error) {
-                boost::asio::async_write(*socket_, boost::asio::buffer(*connection_data_ptr->data), [this, connection_data_ptr](const boost::system::error_code& error, size_t bytes_transferred) {
-                    if (!error) {
-                        do_send(connection_data_ptr->source_id);
-                    }
-                    else {
-                        // Tratar erro
-                    }
-                    });
+                connection_ptr->connected = true;
+                do_send(source_id);
+                do_receive(source_id);
             }
             else {
-                // Tratar erro
+                std::cerr << "Erro ao conectar: " << error.message() << std::endl;
+            }
+            });
+    }
+
+    void do_send(uint32_t source_id) {
+        auto it = connections_.find(source_id);
+        if (it == connections_.end() || !it->second || !it->second->socket || !it->second->connected) {
+            if (it != connections_.end() && it->second && !it->second->connecting && !it->second->connected) {
+                establish_connection(source_id);
+            }
+            return;
+        }
+
+        auto connection_ptr = it->second;
+        if (connection_ptr->connecting) {
+            return;
+        }
+        if (connection_ptr->sending) {
+            return;
+        }
+        if (connection_ptr->send_queue.empty()) {
+            return;
+        }
+
+        connection_ptr->sending = true;
+        auto connection_data_ptr = connection_ptr->send_queue.front();
+        connection_ptr->send_queue.pop();
+
+        boost::asio::async_write(*connection_ptr->socket, boost::asio::buffer(*connection_data_ptr->data), [this, source_id, connection_ptr](const boost::system::error_code& error, size_t bytes_transferred) {
+            connection_ptr->sending = false;
+            if (!error) {
+                do_send(source_id);
+            }
+            else {
+                std::cerr << "Erro ao enviar dados: " << error.message() << std::endl;
+                connection_ptr->connected = false;
+            }
+            });
+    }
+
+    void do_receive(uint32_t source_id) {
+        auto it = connections_.find(source_id);
+        if (it == connections_.end() || !it->second || !it->second->socket || !it->second->connected) {
+            return;
+        }
+
+        auto connection_ptr = it->second;
+        if (connection_ptr->receiving) {
+            return;
+        }
+        connection_ptr->receiving = true;
+
+        connection_ptr->socket->async_read_some(boost::asio::buffer(connection_ptr->receive_buffer), [this, source_id, connection_ptr](const boost::system::error_code& error, size_t bytes_transferred) {
+            connection_ptr->receiving = false;
+            if (!error) {
+                auto receive_buffer = std::make_shared<std::string>(reinterpret_cast<char*>(connection_ptr->receive_buffer.data()), bytes_transferred);
+                receive_handler_(source_id, receive_buffer);
+                do_receive(source_id);
+            }
+            else {
+                std::cerr << "Erro ao receber dados: " << error.message() << std::endl;
             }
             });
     }
 
     boost::asio::io_context& io_context_;
-    std::shared_ptr<boost::asio::ip::tcp::socket> socket_;
-    std::map<uint16_t, std::queue<std::shared_ptr<connection_data>>> send_queue_;
-    std::function<void(uint16_t, uint8_t, std::shared_ptr<std::string>)> receive_handler_;
+    std::map<uint32_t, std::shared_ptr<connection>> connections_;
+    std::function<void(uint32_t, std::shared_ptr<std::string>)> receive_handler_;
 };
 
 internet_connector* internet_connector_instance = nullptr;
@@ -86,14 +173,15 @@ void internet_connector_init(boost::asio::io_context& io_context) {
     }
 }
 
-void internet_connector_async_send(uint16_t source_id, uint8_t proto, uint32_t dest_address, uint16_t dest_port, std::shared_ptr<std::string> data) {
+void internet_connector_async_send(uint32_t source_id, uint8_t proto, uint32_t dest_address, uint16_t dest_port, std::shared_ptr<std::string> data) {
     if (internet_connector_instance) {
         internet_connector_instance->async_send(source_id, proto, dest_address, dest_port, data);
     }
 }
 
-void internet_connector_async_receive(std::function<void(uint16_t, uint8_t, std::shared_ptr<std::string>)> handler) {
+void internet_connector_async_receive(std::function<void(uint32_t, std::shared_ptr<std::string>)> handler) {
     if (internet_connector_instance) {
         internet_connector_instance->async_receive(handler);
     }
 }
+
